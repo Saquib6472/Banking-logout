f29826ba7858a54b4a84f0600d6b45f18fa7b631
Saquib <114405482+Saquib6472@users.noreply.github.com>
2023-10-10 18:24:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 
 const account1 =
 {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Abul saquib',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
